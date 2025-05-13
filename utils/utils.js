b7a3de81d2fb235754c1626dc0ccb084d72e4a4a
Milan Mayur <71451581+MilanMayur@users.noreply.github.com>
2025-05-13 19:34:36 +0530

    Update utils.js

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,7 @@
 export function getSortOption(sort) {
     switch (sort) {
         case 'title': return { title: 1 };
-        case 'rating': return { 'imdb.rating': -1 };
+        case 'rating': return { sortField: { 'imdb.rating': -1 }, isNumericRating: true };
         case 'year': return { released: -1 };
         default: return {};
     }
